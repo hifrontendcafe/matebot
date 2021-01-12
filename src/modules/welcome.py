bff82c3609e8b3d78e769a8f40c9e28f18483459
Leonardo Javier Gago <ljgago@gmail.com>
2021-01-11 21:07:58 -0300

    REFACTOR: Modifico mensaje de bienvenida-consultas

--- src/modules/welcome.py
+++ src/modules/welcome.py
@@ -19,6 +19,7 @@ class Welcome(commands.Cog):
         \nTe cuento, somos una comunidad de personas interesadas en tecnología y ciencias informáticas.\nCharlamos sobre lenguajes de programación, diseño web, infraestructura, compartimos dudas y tratamos de resolverlas juntxs.\nAdemás, nos organizamos en grupos para estudiar, hacer proyectos juntos y charlar en inglés para perfeccionarnos.\nTenemos un espacio de coworking, también nos vamos de after office y jugamos jueguitos!
         \nAcá te dejamos información que **es necesario que revises antes de comenzar a participar**, ya que es muy importante que todxs contribuyamos a mantener este server como un espacio seguro, amigable y divertido para cualquier persona que participe.
         \n* Código de conducta <#748183026244255824>\n* Manual de uso <#747925827265495111>
+        \nPor favor, al hacer una consulta dentro del server, intentá incluir la mayor cantidad de datos que puedas sobre qué estás intentando, qué errores te encuentras y qué quieres lograr para que podamos ayudarte de la mejor manera posible. Si tienes dudas de dónde publicar la pregunta puedes consultar en <#594935077637718027> y te orientarán. Asimismo, puedes usar el buscador, situado arriba a la derecha, para verificar que tu pregunta no hay sido respondida anteriormente.
         \nNos encantaría que pases por <#748547143157022871> y nos cuentes algo de vos :slight_smile:
         \nSaludos!
         \n*El Staff de FrontendCafé*''')
