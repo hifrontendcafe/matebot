7c340f49b3bf1f0bcde409c3f524b215d781635f
Javier Rodriguez <javicerodriguez@gmail.com>
2021-03-13 12:09:21 -0300

    FEATURE: Nuevo módulo NewMembers (#8)

--- src/bot.py
+++ src/bot.py
@@ -51,6 +51,7 @@ if __name__ == "__main__":
     bot.add_cog(modules.Scheduler(bot))
     bot.add_cog(modules.Polls(bot))
     bot.add_cog(modules.Search(bot))
+    bot.add_cog(modules.NewMembers(bot))
 
     log.info("Bot started ...")
     bot.run(TOKEN)
