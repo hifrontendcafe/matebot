41ff6d5aaa880564d9a87737fa6ae8531166133a
Javier Rodriguez <javicerodriguez@gmail.com>
2021-12-14 01:08:08 -0300

    feat: add functions, dateparser and firths responses

--- src/modules/reminders.py
+++ src/modules/reminders.py
@@ -1,7 +1,9 @@
 # Módulo Reminders
 
 import os
+import re
 import logging
+import dateparser
 import discord
 from discord import components
 from discord.ext import commands
@@ -10,6 +12,7 @@ from discord.components import SelectMenu, SelectOption
 import faunadb
 from datetime import datetime as dt
 from libs.database import Database as DB
+from enum import Enum
 
 
 log = logging.getLogger(__name__)
@@ -33,6 +36,14 @@ def buttons():
         )
     ]
 
+
+class Error(Enum):
+    DATETIME        = 1
+    TIMEZONE        = 2
+    DATE_HAS_PASSED = 3
+    CHANNEL         = 4
+
+
 class Reminders(commands.Cog):
     """
     Recordatorios. Los parámetros a pasar serían:\n
@@ -48,7 +59,7 @@ class Reminders(commands.Cog):
         self.PREFIX = os.getenv("DISCORD_PREFIX")
         self.new_reminder = {
             'days': [],
-            'datetime': '',
+            'time': '',
             'channel': '',
             'message': '',
         }
@@ -63,6 +74,14 @@ class Reminders(commands.Cog):
             'Sunday': 'Domingo'
         }   
 
+    @staticmethod
+    def _process_channel(channel):
+        match = re.search(r"\<\#(\d+)\>", channel)
+        if match:
+            channel_id = int(match.group(1))
+            return channel_id
+        else:
+            return Error.CHANNEL
 
     @commands.group()
     async def reminder(self, ctx):
@@ -128,5 +147,59 @@ class Reminders(commands.Cog):
                 self.step = 1
                 self.new_reminder['days'] = select_menu.values
     
+
+        async def t_event(self, ctx):
+            def check_entry(author):
+                def inner_check(message):
+                    return message.author == author
+                return inner_check
+
+            await ctx.send('¿A qué hora (en GMT-3) se deberá publicar el recordatorio? Presione "Continuar" para avanzar', delete_after=180)
+            msg = await self.bot.wait_for('message', check=check_entry(ctx.author))
+            dtime = dateparser.parse(msg.content)
+            if dtime != None:
+                self.step = 2
+                self.new_reminder['time'] = dtime.time()
+
+
+        async def ch_event(self, ctx):
+            def check_entry(author):
+                def inner_check(message):
+                    return message.author == author
+                return inner_check
+            
+            await ctx.send('¿En qué canal quieres publicar el recordatorio?', delete_after=180)
+            msg = await self.bot.wait_for('message', check=check_entry(ctx.author))
+            channel_id = self._process_channel(msg.content)
+            if channel_id is Error.CHANNEL:
+                embed = Embed(
+                    title="Error",
+                    description="Por favor, elija un canal válido.",
+                    color=discord.Colour.green().value
+                )
+                return await ctx.send(embed=embed, delete_after=60)
+            else:
+                self.step = 3
+                self.new_reminder['channel'] = f'<#{channel_id}>'
+
         await days(self, ctx)
         await ctx.send(str(self.new_reminder['days']))
+
+        await t_event(self, ctx)
+        await ctx.send(str(self.new_reminder['time']))
+
+        await ch_event(self, ctx)
+        await ctx.send(str(self.new_reminder['channel']))
+
+        embed = Embed(
+            title="Nuevo recordatorio",
+            description="Sarasa",
+            color=discord.Colour.green().value
+        )
+
+        for key in self.new_reminder.keys():
+            if key != 'message':
+                embed.add_field(name=key, value=self.new_reminder[key], inline=True)
+        embed.set_footer(text="Los datos son correctos?")
+
+        await ctx.send(embed=embed, delete_after=600)
